d838d3534d42bb7b96246cf558e5138cd640e96e
unknown <darshanc679@gmail.com>
2022-01-31 18:42:29 +0530

    variables

---
+++ variables.php
@@ -0,0 +1,55 @@
+<?php 
+
+
+
+$q1=1;
+
+echo "$q1  <br>";
+
+$q2=2;
+
+echo "$q2<br>";
+
+$q1=$q2;
+
+echo "$q1  <br>";
+
+echo "$q2
+  <br>";
+
+
+
+
+
+
+
+
+
+/*
+  class student{
+  protected $num = 2;
+
+  public function setnum($num)
+  {
+     $this->$num = $num;
+     return $this;
+  }
+
+  public function getnum()
+  {
+     return $this->$num;
+  }
+ }
+ 
+ $s1 = new student();
+
+ $a1=$s1->setnum(4);
+ print_r($a1);
+
+ $a2=$s1->getnum();
+ print_r($a2);*/
+
+  
+
+
+?>
